b21ea51817c37e5d33ae7e9c09c4f653c3845d77
TrAyZeN <1810leo@gmail.com>
2021-05-22 00:06:31 +0200

    Enable lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![warn(
+    missing_debug_implementations,
+    rust_2018_idioms,
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+
 fn main() {
     println!("Hello, world!");
 }
