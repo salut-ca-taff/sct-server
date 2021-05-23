6ea94289be1ae689333bf1a9984d0dfff269ea2a
TrAyZeN <1810leo@gmail.com>
2021-05-23 07:57:20 +0200

    Add query to fill data

---
+++ data.sql
@@ -0,0 +1,17 @@
+INSERT INTO subjects ( slug, title )
+VALUES ( "maths", "Mathematique" );
+
+INSERT INTO subjects ( slug, title )
+VALUES ( "francais", "Francais" );
+
+INSERT INTO subjects ( slug, title )
+VALUES ( "histoire", "Histoire" );
+
+INSERT INTO subjects ( slug, title )
+VALUES ( "geo", "Geographie" );
+
+INSERT INTO subjects ( slug, title )
+VALUES ( "physique", "Physique" );
+
+INSERT INTO subjects ( slug, title )
+VALUES ( "chimie", "Chimie" );
